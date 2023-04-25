8a3c8ba371a88464809925dcc92b167ffc952069
manasabathini <33703157+manasabathini@users.noreply.github.com>
2023-04-25 00:25:29 +0000

    5th day of code

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Marvel Movie Character Creator")
+print("--")
+hangingAround = input("Do you like 'hanging around'?: ") 
+if hangingAround == "No" :
+  print("Then you're not Spider-man")
+else:
+  print("You are a Spider-man")
+  print()
+gravelly = input("Do you have a 'gravelly' voice?: ")
+if gravelly == "No":
+  print("Aww, then you're not Korg")
+else:
+  print("You are Krog")
+  print()
+marvelous = input("Do you often feel 'Marvelous'?: ")
+
+if marvelous == "Yes":
+  print("Aha! You're Captain Marvel! Hi!")
+else:
+  print("You are not Captain Marvel")
